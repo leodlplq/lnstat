a5e4d3685a549232493714d5cd70f9ef5dd9db7d
zonzzlive <zonzz24@gmail.com>
2022-03-13 16:07:53 +0100

    :hammer: add league for a game

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import bg from "../assets/images/background/bg-main.png"
 
 export default function Home(){
       return (
             <div className="home">
-                  <h1>Home</h1>
+                  <div className="header-home">
+                        <div className="right">
+                              <h1>Pariez et suivez l'actualité <br></br> de vos jeux préférés </h1>
+                        </div>
+                        <div className="left">
+                              <img src={bg} alt="Logo de lnstat" className=""/>
+                        </div>
+                  </div>
             </div>
+
+
       )
 }
--- src/components/league/League.js
+++ src/components/league/League.js
@@ -1,120 +1,85 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import LeagueCard from "./LeagueCard";
 
-import logo from "../../assets/images/logo.png"
-import bgf from "../../assets/images/games/rocket-league.jpg"
-
-const ORIGIN = `https://api.pandascore.co/`
+const ORIGIN = `https://api.pandascore.co/`;
 const ORIGINDB = `${window.location.protocol}//${window.location.hostname}:3000`
 const TOKEN = process.env.REACT_APP_PANDASCORE_TOKEN
 
-
-
 export default function League(){
       
-      let { slug } = useParams()
-      let { bg } = useParams()
-      const [leagues, setLeagues] = useState([])
-      const [videogame, setVideogame] = useState([{image_url:"valorant.jpg"}])
-      const [formData, setFormData] = useState("")
-
-      //console.log(slug)
-      //console.log(leagues)
-
-      async function getLeagueBySlug(page, perPage, slug){
-
-            var myHeaders = new Headers();
-            myHeaders.append("Authorization", `Bearer ${TOKEN}`);
-
-            var requestOptions = {
-                  method: 'GET',
-                  headers: myHeaders,
-                  redirect: 'follow'
-            };
-
-            fetch(`${ORIGIN}videogames/${slug}/leagues`, requestOptions)
-                  .then(response => response.json())
-                  .then(result => setLeagues(result))
-                  .catch(error => console.log('error', error));
-      }
-
-      async function getSearchedLeaguesData(page, perPage, slug){
-            var myHeaders = new Headers();
-            myHeaders.append("Authorization", `Bearer ${TOKEN}`);
-
-            var requestOptions = {
-                  method: 'GET',
-                  headers: myHeaders,
-                  redirect: 'follow'
-            };
-
-            fetch(`${ORIGIN}videogames/${slug}/leagues?search[name]=${formData}&sort=&page=1&per_page=50`, requestOptions)
-                  .then(response => response.json())
-                  .then(result => setLeagues(result))
-                  .catch(error => console.log('error', error));
-      }
-
-      async function getVideogame(slug){
-
-            var myHeaders = new Headers();
-            myHeaders.append("Authorization", `Bearer ${TOKEN}`);
-
-            var requestOptions = {
-                  method: 'GET',
-                  headers: myHeaders,
-                  redirect: 'follow'
-            };
-
-            fetch(`${ORIGINDB}/games?slug=${slug}`, requestOptions)
-                  .then(response => response.json())
-                  .then(result => setVideogame(result))
-                  .catch(error => console.log('error', error));
-      }
-
-      function handleSubmit(e) {
-            e.preventDefault()
-            getSearchedLeaguesData()
-      }
-
-      function handleChange(e) {
-            setFormData(e.target.value)
-      }
-
-      useEffect(() => {
-
-            if(formData === ""){
-                  getLeagueBySlug(1,100, slug);
-            } else {
-                  getSearchedLeaguesData(1, 100, slug)
-            }
+    let { slug } = useParams();
+    const [videogame, setVideogame] = useState([{image_url:"valorant.jpg"}])
+    const [league, setLeague] = useState({
+        current_videogame: {
+            slug:"loading"
+        },
             
-      }, [formData]);
+    });
+
+    async function getLeagueBySlug(slug){
+
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${TOKEN}`);
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        fetch(`${ORIGIN}leagues/${slug}`, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                setLeague(result)
+                getVideogame(league.current_videogame.slug)
+            })
+            .catch(error => console.log('error', error));
+      }
       
-      console.log(videogame[0].slug)
+    async function getVideogame(slug){
+
+    var myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${TOKEN}`);
+
+        var requestOptions = {
+              method: 'GET',
+              headers: myHeaders,
+              redirect: 'follow'
+        };
+
+        fetch(`${ORIGINDB}/games?slug=${slug}`, requestOptions)
+              .then(response => response.json())
+              .then(result => setVideogame(result))
+              .catch(error => console.log('error', error));
+    }
+
+    useEffect(() => {
+        getLeagueBySlug(slug)
+        getVideogame(league.current_videogame.slug)
+    }, []);
+      
+    
+    /*console.log(videogame[0])
+    console.log(slug)
+    console.log(league)*/
 
       return (
-            <div className="league-game">
+            <div className="league-unique">
                   <div className="league-herobanner">
                         <div className="league-background">
-                              <img src={require(`../../assets/images/games/${videogame[0].image_url}`)} alt="Valorant"/>
-                              <div className="overlay"></div>
+                        <img src={require(`../../assets/images/games/${videogame[0].image_url}`)} alt="Valorant"/>
+                            <div className="overlay"></div>
                         </div>
-                        <div className="league-logo">
-                              <h1 className="league-name">{slug}</h1>
+                        <div className="league-infos">
+                            <div className="league-logo">
+                                    <span>X</span>
+                                    
+                            </div>
+                            <h1 className="league-name">{league.name}</h1>
                         </div>
+                        
                   </div>
-                  <div className="leagues">
-                        <div className="header-leagues">
-                              <h1 className="leagues-title">Les ligues de {slug}</h1>
-                              <form onSubmit={handleSubmit}>
-                                    <input type="text" className="input-form" placeholder="Search" onChange={handleChange}/>
-                              </form>
-                        </div>
-                        <div className="league-container">
-                              {leagues.length !== 0 ? leagues.map(el=><LeagueCard key={el.id} slugLeague={el.slug} name={el.name} imageUrl={el.image_url} leagueGame={el.videogame.name}/>) : "loading"}                      
-                        </div>
-                  </div>
+                  
             </div>
       )
 }
